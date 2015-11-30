e9d620fe26ca98767b77e6886548d1916f91352c
EC2 Default User <ec2-user@ip-172-31-16-48.us-west-2.compute.internal>
2015-11-30 18:19:19 +0000

    server

--- mail_send.rb
+++ mail_send.rb
@@ -1,11 +1,26 @@
 require 'sinatra'
-require 'rest-client'
-get '/' do
-  RestClient.post "https://api:key-5go59arglz5e8yvw32ai9ycmwvr557t0"\
-    "@api.mailgun.net/v3/apart.mailgun.org/messages",
-    :from => "Excited User <mailgun@apart.mailgun.org>",
-    :to => "bar@example.com, minyoung9353@snu.ac.kr",
-    :subject => "Hello",
-    :text => "Testing some Mailgun awesomness!"
+require 'mail'
 
+get '/' do
+=begin
+	Mail.defaults do
+	  delivery_method :smtp, {
+	    :port      => 587,
+	    :address   => "smtp.mailgun.com",
+	    :user_name => "postmaster@apart.mailgun.org",
+	    :password  => "4813b0b2c30dcf1a097e94c5f18d45d7",
+	  }
+	end
+
+	mail = Mail.deliver do
+	  to      'minyoung9353@gmail.com'
+	  from    'admin@apart.mailgun.org'
+	  subject 'Hello'
+
+	  text_part do
+	    body 'Testing some Mailgun awesomness'
+	  end
+	end
+=end
+	"success"
 end
