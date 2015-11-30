b0bc4085afca0af325a192b05ffc1f380b87254a
EC2 Default User <ec2-user@ip-172-31-16-48.us-west-2.compute.internal>
2015-11-30 18:38:16 +0000

    mail2

--- mail_send.rb
+++ mail_send.rb
@@ -1,26 +1,33 @@
 require 'sinatra'
 require 'mail'
-
-get '/' do
-=begin
+post '/' do
 	Mail.defaults do
 	  delivery_method :smtp, {
 	    :port      => 587,
 	    :address   => "smtp.mailgun.com",
 	    :user_name => "postmaster@apart.mailgun.org",
 	    :password  => "4813b0b2c30dcf1a097e94c5f18d45d7",
+	openssl_verify_mode: 'none'
 	  }
 	end
+	if params[:fix].to_i == 0
+		my_subject = "[집고치기에 관심]" + params[:name]
+	else
+		my_subject = "[파트너쉽에 관심]" + params[:name]
+	end
+
+	content = params[:content]
 
 	mail = Mail.deliver do
-	  to      'minyoung9353@gmail.com'
+	  to      'apartmentary@naver.com'
 	  from    'admin@apart.mailgun.org'
-	  subject 'Hello'
+	  subject my_subject
 
 	  text_part do
-	    body 'Testing some Mailgun awesomness'
+	    body content 
 	  end
 	end
-=end
+
 	"success"
 end
+set :environment, :production
